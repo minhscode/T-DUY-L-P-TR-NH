ed70316c7a85fcbcd5044926d345784063b71422
Ming no code <tranminh156158@gmail.com>
2025-11-02 15:32:20 +0700

    Update hiengiaodien.py

--- TU DUY LAP TRINH/hiengiaodien.py
+++ TU DUY LAP TRINH/hiengiaodien.py
@@ -1,71 +1,106 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox, QListView
+
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
 from untitled import *
+from xuly import *
 from PyQt6.QtCore import QStringListModel
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.bntremove.hide()
-        self.ui.buttonchangetodone.hide()
-        self.ui.buttonremoveindone.hide()
-        self.ui.buttonchangebacktotodolist.hide()
-        self.connection()
-    def show(self):
-        super().show()
-    def connection(self):
-    #Well well well button to change page
-        self.ui.todolistbutton.clicked.connect(lambda: self.home_page(0) )
-        self.ui.donebutton.clicked.connect(lambda: self.home_page(1) )
-        self.ui.quaylaitrangtodolist.clicked.connect(lambda: self.done_page(0)) #QUAY LAI TRANG BAN DAU
-    #Button to interact with the page
-        self.ui.bntadd.clicked.connect(self.add_item)
-        self.ui.bntremove.clicked.connect(self.remove_item)
-        self.ui.todolist_list.clicked.connect(self.show_remove_item) #KHI BAM VAO LIST SE HIEN CHUC NANG
-        self.ui.buttonchangetodone.clicked.connect(self.additemtodone)
-        self.ui.buttonchangebacktotodolist.clicked.connect(self.add_to_todolist)
-        self.ui.buttonremoveindone.clicked.connect(self.delete_item)
-        self.ui.done_list.clicked.connect(self.show_delete)
-    #Todolist page
+        self.ui.removebut.hide()
+        self.ui.button_to_done.hide()
+        self.ui.remarkablebut.hide()
+        self.ui.removebutprior.hide()
+        self.ui.removebutdone.hide()
+        self.ui.button_to_todo.hide()
+        self.ui.unmarkbutton.hide()
+        #self.ui.editbut.hide()
+
+        # Todolist page
         self.todolist = QStringListModel()
         datatodolist = []
         self.todolist.setStringList(datatodolist)
-        self.ui.todolist_list.setModel(self.todolist)
+        self.ui.listcv.setModel(self.todolist)
         self.todolist_index = None
-        self.donelist_index = None
-    #Done page
-        self.tasktracker = QStringListModel()
+
+        # Done page
+        self.done = QStringListModel()
         datadone = []
-        self.tasktracker.setStringList(datadone)
-        self.ui.done_list.setModel(self.tasktracker)
+        self.done.setStringList(datadone)
+        self.ui.listcvhoanthanh.setModel(self.done)
+        self.donelist_index = None
+        #RANDOM CÂU TRUYỀN ĐỘNG LỰC
+        randoms = "No"
+        self.ui.linetruyendongluc.setText(motivation(randoms))
+        self.connection()
+        #PRIORITY
+        self.prioritytask = QStringListModel()
+        dataprior = []
+        self.prioritytask.setStringList(dataprior)
+        self.ui.listcvquantrong.setModel(self.prioritytask)
+        self.prior_index = None
+
+    def show(self):
+        super().show()
+
+    def connection(self):
+
+    #Well well well button to change page
+        self.ui.todolistbutton.clicked.connect(lambda: self.home_page(0) )
+        self.ui.donebutton.clicked.connect(lambda: self.home_page(1) )
+
+    #Button to interact with the page
+        self.ui.addbut.clicked.connect(self.add_item)
+        self.ui.removebut.clicked.connect(self.remove_item)
+        self.ui.listcv.clicked.connect(self.show_item) #KHI BAM VAO LIST SE HIEN CHUC NANG
+        self.ui.button_to_done.clicked.connect(self.additemtodone)
+        self.ui.button_to_todo.clicked.connect(self.add_to_todolist)
+        self.ui.removebutdone.clicked.connect(self.delete_item)
+        self.ui.listcvhoanthanh.clicked.connect(self.show_delete)
+        self.ui.remarkablebut.clicked.connect(self.add_to_prior)
+        self.ui.unmarkbutton.clicked.connect(self.add_back_todolist)
+        self.ui.listcvquantrong.clicked.connect(self.show_prior)
+        self.ui.removebutprior.clicked.connect(self.erase_item_in_prior)
 
     #TO DO LIST FUNCTION:
     def add_item(self):
 
-        adding = self.ui.add.text()
+        adding = self.ui.fill.text()
         current_list = self.todolist.stringList()
         current_list.append(adding)
         self.todolist.setStringList(current_list)
+
     #LUU VI TRI DE XOA/CHUYEN DU LIEU QUA DONE
-    def show_remove_item(self, index):
-        self.ui.bntremove.show()
-        self.ui.buttonchangetodone.show()
+    def show_item(self, index):
+
+        self.ui.removebut.show()
+        self.ui.button_to_done.show()
+        self.ui.remarkablebut.show()
         self.todolist_index = index
+
     #CHUC NANG XOA
     def remove_item(self):
+
         current_list = self.todolist.stringList()
         if self.todolist_index is None:
             return
         del current_list[self.todolist_index.row()]
         self.todolist.setStringList(current_list)
-        self.ui.bntremove.hide()
-        self.ui.buttonchangetodone.hide()
+        self.ui.removebut.hide()
+        self.ui.button_to_done.hide()
+        self.ui.remarkablebut.hide()
+
     #CHUYEN DU LIEU QUA DONE
     def additemtodone(self):
-        done_list = self.tasktracker.stringList()
+
+        random = "Yes"
+        self.ui.linetruyendongluc.setText(motivation(random))
+        done_list = self.done.stringList()
         to_do_list = self.todolist.stringList()
         if self.todolist_index is None:
             return
@@ -73,35 +108,86 @@ class MainWindow(QMainWindow):
         done_list.append(value)
         del to_do_list[self.todolist_index.row()]
         self.todolist.setStringList(to_do_list)
-        self.tasktracker.setStringList(done_list)
-        self.ui.bntremove.hide()
-        self.ui.buttonchangetodone.hide()
+        self.done.setStringList(done_list)
+        self.ui.removebut.hide()
+        self.ui.button_to_done.hide()
+        self.ui.remarkablebut.hide()
+        self.ui.listcv.clearSelection()
+
+    def add_to_prior(self):
+
+        if self.todolist_index is None:
+            return
+        todolist = self.todolist.stringList()
+        priorlist = self.prioritytask.stringList()
+        priorlist.append(self.todolist_index.data())
+        del todolist[self.todolist_index.row()]
+        self.todolist.setStringList(todolist)
+        self.prioritytask.setStringList(priorlist)
+        self.ui.removebut.hide()
+        self.ui.remarkablebut.hide()
+        self.ui.button_to_done.hide()
+
+    #PRIORITY FUNCTION:
+    def show_prior(self, index):
+
+        self.prior_index = index
+        self.ui.unmarkbutton.show()
+        self.ui.removebutprior.show()
+
+    def add_back_todolist(self):
+
+        if self.prior_index is None:
+            return
+        todolist = self.todolist.stringList()
+        priorlist = self.prioritytask.stringList()
+        todolist.append(self.prior_index.data())
+        del priorlist[self.prior_index.row()]
+        self.todolist.setStringList(todolist)
+        self.prioritytask.setStringList(priorlist)
+        self.ui.unmarkbutton.hide()
+        self.ui.removebutprior.hide()
+
+    def erase_item_in_prior(self):
+
+        if self.prior_index is None:
+            return
+        priorlist = self.prioritytask.stringList()
+        del priorlist[self.prior_index.row()]
+        self.prioritytask.setStringList(priorlist)
+        self.ui.removebutprior.hide()
+        self.ui.unmarkbutton.hide()
+
     #DONE FUNCTION:
     def show_delete(self, index):
+
         self.donelist_index = index
-        self.ui.buttonremoveindone.show()
-        self.ui.buttonchangebacktotodolist.show()
+        self.ui.removebutdone.show()
+        self.ui.button_to_todo.show()
+
     def delete_item(self):
+
         if self.donelist_index is None:
             return
-        done_List = self.tasktracker.stringList()
+        done_List = self.done.stringList()
         del done_List[self.donelist_index.row()]
-        self.tasktracker.setStringList(done_List)
-        self.ui.buttonchangebacktotodolist.hide()
-        self.ui.buttonremoveindone.hide()
+        self.done.setStringList(done_List)
+        self.ui.button_to_todo.hide()
+        self.ui.removebutdone.hide()
+
     def add_to_todolist(self):
+
         todolist_list = self.todolist.stringList()
-        done_list = self.tasktracker.stringList()
+        done_list = self.done.stringList()
         if self.donelist_index is None:
             return
         value = self.donelist_index.data()
         todolist_list.append(value)
         del done_list[self.donelist_index.row()]
         self.todolist.setStringList(todolist_list)
-        self.tasktracker.setStringList(done_list)
-        self.ui.buttonremoveindone.hide()
-        self.ui.buttonchangebacktotodolist.hide()
-
+        self.done.setStringList(done_list)
+        self.ui.removebutdone.hide()
+        self.ui.button_to_todo.hide()
 
     #TRANG CHINH
     def home_page(self, index):
@@ -115,3 +201,4 @@ if __name__ == "__main__":
     window = MainWindow()
     window.show()
     app.exec()
+
